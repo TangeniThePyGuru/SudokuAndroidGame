f5213aab0926056cad16159a16dc295e9f42e281
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-03-19 03:37:55 -0400

    App interface updated with getBoard and three new buttons added.

--- app/src/main/java/com/example/sudokuapp2/AppInterface.java
+++ app/src/main/java/com/example/sudokuapp2/AppInterface.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.graphics.Color;
@@ -18,6 +19,8 @@ public class AppInterface extends GridLayout
     private EditText[][] board;
     public TextView message;
     public Button btnNewGame;
+    public Button btnSaveGame;
+    public Button btnResumeGame;
 //    private int size;
     public AppInterface(Context context, int size, int width)
     {
@@ -61,20 +64,59 @@ public class AppInterface extends GridLayout
         message.setLayoutParams(params);
         addView(message);
 
+        // new game button
         btnNewGame = new Button(context);
+        btnNewGame.setId(generateViewId());
         btnNewGame.setGravity(Gravity.CENTER_HORIZONTAL);
-        btnNewGame.setPadding(2,2,2,2);
+//        btnNewGame.setPadding(2,2,2,2);
         btnNewGame.setTextAlignment(TEXT_ALIGNMENT_CENTER);
         btnNewGame.setText("NEW GAME");
+        btnNewGame.setBackgroundColor(Color.RED);
         params = new GridLayout.LayoutParams();
-        params.width = LayoutParams.WRAP_CONTENT;
-        params.height = LayoutParams.WRAP_CONTENT;
-        params.rowSpec = GridLayout.spec(size, 1);
-        params.columnSpec = GridLayout.spec(0, size);
-        params.topMargin = params.bottomMargin = 5;
-        params.leftMargin = params.rightMargin = 5;
+        params.width = width * 3;
+        params.height = width;
+        params.rowSpec = GridLayout.spec(size + 1, 1);
+        params.columnSpec = GridLayout.spec(0, 3);
+//        params.topMargin = params.bottomMargin = 5;
+//        params.leftMargin = params.rightMargin = 5;
         btnNewGame.setLayoutParams(params);
         addView(btnNewGame);
+
+        // save game button
+        btnSaveGame = new Button(context);
+        btnSaveGame.setId(generateViewId());
+        btnSaveGame.setGravity(Gravity.CENTER_HORIZONTAL);
+//        btnSaveGame.setPadding(2,2,2,2);
+        btnSaveGame.setTextAlignment(TEXT_ALIGNMENT_CENTER);
+        btnSaveGame.setText("SAVE GAME");
+        btnSaveGame.setBackgroundColor(Color.BLUE);
+        params = new GridLayout.LayoutParams();
+        params.width = width * 3;
+        params.height = width;
+        params.rowSpec = GridLayout.spec(size + 1, 1);
+        params.columnSpec = GridLayout.spec(3, 3);
+//        params.topMargin = params.bottomMargin = 5;
+//        params.leftMargin = params.rightMargin = 5;
+        btnSaveGame.setLayoutParams(params);
+        addView(btnSaveGame);
+
+        // resume game button
+        btnResumeGame = new Button(context);
+        btnResumeGame.setId(generateViewId());
+        btnResumeGame.setGravity(Gravity.CENTER_HORIZONTAL);
+//        btnResumeGame.setPadding(2,2,2,2);
+        btnResumeGame.setTextAlignment(TEXT_ALIGNMENT_CENTER);
+        btnResumeGame.setText("RESUME GAME");
+        btnResumeGame.setBackgroundColor(Color.GREEN);
+        params = new GridLayout.LayoutParams();
+        params.width = width * 3;
+        params.height = width;
+        params.rowSpec = GridLayout.spec(size + 1, 1);
+        params.columnSpec = GridLayout.spec(6, 3);
+//        params.topMargin = params.bottomMargin = 5;
+//        params.leftMargin = params.rightMargin = 5;
+        btnResumeGame.setLayoutParams(params);
+        addView(btnResumeGame);
     }
 
     public void drawInitialBoard(int[][] board)
@@ -86,6 +128,25 @@ public class AppInterface extends GridLayout
                     this.board[i][j].setText("");
                     this.board[i][j].setEnabled(true);
                 } else {
+                    this.board[i][j].setText(board[i][j]+"");
+                    this.board[i][j].setBackgroundColor(Color.LTGRAY);
+                    this.board[i][j].setEnabled(false);
+                }
+            }
+        }
+    }
+
+    // draw the saved game onto the board
+    public void drawSavedBoard(int[][] board){
+        for (int i = 0; i < this.board.length; i++){
+            for (int j = 0; j < this.board.length; j++){
+                // fill the players cells with number
+                if (Storage.signs[i][j].equals("+")) {
+                    this.board[i][j].setText(board[i][j]+"");
+                    this.board[i][j].setEnabled(true);
+                    this.board[i][j].setBackgroundColor(Color.parseColor("#009900"));
+                } else {
+                    // fill the cells with generated numbers
                     this.board[i][j].setText(board[i][j]+"");
                     this.board[i][j].setBackgroundColor(Color.LTGRAY);
                     this.board[i][j].setEnabled(false);
@@ -109,5 +170,9 @@ public class AppInterface extends GridLayout
     {
         //clear input at x, y
         board[x][y].setText("");
+    }
+
+    public EditText[][] getBoard() {
+        return this.board;
     }
 }
