989a2111d7e7b3508d9ff4e4fad879db95c53dc4
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-03-19 03:30:03 -0400

    file storage class updated

--- app/src/main/java/com/example/sudokuapp2/Storage.java
+++ app/src/main/java/com/example/sudokuapp2/Storage.java
@@ -1,62 +1,89 @@
 package com.example.sudokuapp2;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Scanner;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.preference.PreferenceManager;
+import android.widget.EditText;
 
 public class Storage {
 
+    private Context context;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    // store signs in this array, + means that the number belongs to the
+    public static String[][] signs = new String[9][9];
+
+    public Storage(Context context){
+        this.context = context;
+        preferences = PreferenceManager.getDefaultSharedPreferences(this.context);
+        editor = preferences.edit();
+    }
+
     public int[][] readSavedGame(){
 
+        // initialize the an array to store the data
         int[][] game = new int[9][9];
 
-        try {
+        // read the persisted data
+        String savedGame = preferences.getString("GAME", "");
 
-            File myObj = new File("sudoku.txt");
-            Scanner myReader = new Scanner(myObj);
-            int row = 0;
-            while (myReader.hasNextLine()) {
-                String[] data = myReader.nextLine().split(",");
-                for (int j = 0; j < game.length; j++){
-                    game[row][j] = Integer.parseInt(data[j]);
-                }
-                row++;
+        String[] rows = savedGame.split("\n");
+        // loop through the rows
+        for (int i = 0; i < rows.length; i++){
+            String[] singleRow;
+            singleRow = rows[i].split(",");
+            // loop through the row
+            for (int j = 0; j < singleRow.length; j++){
+                // players number
+                if (singleRow[j].substring(0,1) == "+")
+                    game[i][j] = Integer.parseInt(singleRow[j].substring(1));
+                // generated numbers
+                else
+                    game[i][j] = Integer.parseInt(singleRow[j].substring(1));
             }
-
-
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
         }
 
         return game;
-
     }
 
-    public void saveGame(int[][] game){
+    public void saveGame(int[][] game, EditText[][] gameBoard){
+        String row = "";
 
-        try {
-
-            File myObj = new File("sudoku.txt");
-            String row = "";
-            // delete the file if it exists already
-            if (myObj.exists()) myObj.delete();
-
-            if (myObj.createNewFile()) {
-                for (int i = 0; i < game.length; i++){
-                    for (int j = 0; j < game.length; j++){
-                        row += game[i][j] == 0 ? 0 : game[i][j] +",";
+        // clear all the existing data
+        editor.clear();
+        for (int i = 0; i < game.length; i++){
+            for (int j = 0; j < game.length; j++){
+                // all the zeros belong to the player
+                if (game[i][j] == 0){
+                    row += "+"+game[i][j];
+                    signs[i][j] = "+";
+                } else {
+                    // all the non-zeros in a gray area were generated
+                    if (((ColorDrawable) gameBoard[i][j].getBackground()).getColor() == Color.LTGRAY) {
+                        row += "-" + game[i][j];
+                        signs[i][j] = "-";
                     }
-                    row += "\n";
+                    // all the numbers in the green colored boxes belong to the player
+                    else {
+                        row += "+" + game[i][j];
+                        signs[i][j] = "+";
+                    }
                 }
+                // no comma should appear at the end of a row
+                if( j + 1 < game.length)
+                    row += ",";
             }
-
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            // no new line char should appear at the end of the last row
+            if( i + 1 < game.length)
+                row += "\n";
         }
+
+        // save the game to the persistent memory
+        editor.putString("GAME", row);
+        // persist the game
+        editor.apply();
     }
 
 }
